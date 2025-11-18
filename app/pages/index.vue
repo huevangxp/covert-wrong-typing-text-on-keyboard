d8d5773992c842f16708cd2a5de1c14373f15b4d
huevangxp <huevang933@gmail.com>
2025-11-18 15:59:15 +0700

    commit Tue 11/18/2025 15:59:15.20

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -14,10 +14,7 @@
        <v-btn @click="convert">Convert</v-btn>
     </v-container>
     <v-container>
-        <v-row>
-            <v-col cols="12">
-                <v-textarea v-model="text" label="Text" />
-            </v-col>
-        </v-row>
+         <v-card-title>Result</v-card-title>
+         <v-card-text>{{ result }}</v-card-text>
     </v-container>
 </template>
