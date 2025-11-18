0361d02aa4f6146177eaff1cbdcddbf98aeb90c4
huevangxp <huevang933@gmail.com>
2025-11-18 15:58:01 +0700

    commit Tue 11/18/2025 15:58:01.20

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="text-center">
-       <v-img src="https://lullar-de-3.appspot.com/images/lullar_de_logo.png" />
+       <v-img src="https://lullar-de-3.appspot.com/images/lullar_de_logo.png" width="200" height="200" />
     </div>
 </template>
