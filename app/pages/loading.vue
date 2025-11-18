b94078d5a142f0cb1543e26939dd29a51c09cd78
huevangxp <huevang933@gmail.com>
2025-11-18 18:07:55 +0700

    commit Tue 11/18/2025 18:07:55.13

--- app/pages/loading.vue
+++ app/pages/loading.vue
@@ -6,6 +6,11 @@
         <span></span>
         <span></span>
         <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
     </div>
   </template>
@@ -35,6 +40,11 @@
   .loading span:nth-child(3) { background-color: #00b3a4; animation-delay: 0.2s; }
   .loading span:nth-child(4) { background-color: #4dd0c5; animation-delay: 0.3s; }
   .loading span:nth-child(5) { background-color: #80ece4; animation-delay: 0.4s; }
+  .loading span:nth-child(6) { background-color: #007a74; animation-delay: 0.5s; }
+  .loading span:nth-child(7) { background-color: #009688; animation-delay: 0.6s; }
+  .loading span:nth-child(8) { background-color: #00b3a4; animation-delay: 0.7s; }
+  .loading span:nth-child(9) { background-color: #4dd0c5; animation-delay: 0.8s; }
+  .loading span:nth-child(10) { background-color: #80ece4; animation-delay: 0.9s; }
   
   /* Smooth scale effect */
   @keyframes scale {
