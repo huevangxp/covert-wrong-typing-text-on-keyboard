05e295c9b7e4e3c57b2dd4ff4a7885a3918829a7
huevangxp <huevang933@gmail.com>
2025-11-18 15:58:13 +0700

    commit Tue 11/18/2025 15:58:13.13

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="text-center">
-       <v-img src="https://lullar-de-3.appspot.com/images/lullar_de_logo.png" width="200" height="200" />
+       <v-img src="https://lullar-de-3.appspot.com/images/lullar_de_logo.png" width="200" height="200"  class="mx-auto"/>
+       <h1>Convert English to Lao</h1>
     </div>
 </template>
