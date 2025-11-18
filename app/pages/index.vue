6377c3a20d28bc6477b54bab966d09b5e4258bb2
huevangxp <huevang933@gmail.com>
2025-11-18 16:53:02 +0700

    commit Tue 11/18/2025 16:53:02.13

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,7 +8,7 @@
     <v-container>
         <v-row>
             <v-col cols="12">
-                <v-textarea v-model="text" label="Text" />
+                <v-textarea v-model="text" label="ຂໍ້ຄວາມ" />
             </v-col>
         </v-row>
         <v-btn @click="convert">ປ່ຽນພາສາ</v-btn>
