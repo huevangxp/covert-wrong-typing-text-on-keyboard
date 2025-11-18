a65d659cd859117c2836e1b67929e26e86b84459
huevangxp <huevang933@gmail.com>
2025-11-18 16:04:52 +0700

    commit Tue 11/18/2025 16:04:52.17

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,26 @@
+<template>
+    <div class="text-center">
+        <v-img src="https://lullar-de-3.appspot.com/images/lullar_de_logo.png" width="200" height="200"
+            class="mx-auto" />
+        <v-card-title class="mt-4">ແປປະໂຫຍກ ຫຼື ຂໍ້ຄວາມທີ່ພິມຜິດຈາກພາສາອັງກິດໃຫ້ເປັນພາສາລາວ
+            ເມື່ອລືມກົດປ່ຽນພາສາ</v-card-title>
+    </div>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <v-textarea v-model="text" label="Text" />
+            </v-col>
+        </v-row>
+        <v-btn @click="convert">ປ່ຽນພາສາ</v-btn>
+    </v-container>
+    <v-container>
+        <v-card class="mt-4">
+            <v-card-title>ຜິດຜິດທີ່ປ່ຽນພາສາ:</v-card-title>
+            <v-card-text>{{ result }}</v-card-text>
+        </v-card>
+    </v-container>
+</template>
+
 <script setup>
 const text = ref('')
 const result = ref('')
