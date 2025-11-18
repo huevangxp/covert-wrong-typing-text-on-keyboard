e3c09ccbf6f2366d15d91d0d189887e07c593ce7
huevangxp <huevang933@gmail.com>
2025-11-18 16:55:23 +0700

    commit Tue 11/18/2025 16:55:23.19

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -12,7 +12,7 @@
             </v-col>
         </v-row>
         
-        <v-btn @click="convert" class="bg-primary">ປ່ຽນພາສາ</v-btn>
+        <v-btn @click="convert" class="bg-primary font-weight-bold">ປ່ຽນພາສາ</v-btn>
     </v-container>
     <v-container>
         <v-card class="mt-4">
