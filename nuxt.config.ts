81ebafc3cc741edd5e6dcd656ddf9f9bced1785a
huevangxp <huevang933@gmail.com>
2025-11-18 15:45:53 +0700

    commit Tue 11/18/2025 15:45:53.12

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
   app: {
     head: {
       title: 'My App',
-      link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://cdn.jsdelivr.net/npm/vuetify@3.2.17/dist/vuetify.min.css',
-        },
-      ],
     },
   },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  
   build: {
     transpile: ['vuetify'],
   },
+  
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+  
   vite: {
-    plugins: [
-      // @ts-expect-error
-      vuetify({ autoImport: true }),
-    ],
     vue: {
       template: {
         transformAssetUrls,
       },
     },
   },
-
+  
   devServer: {
     port: 4040,
     host: '0.0.0.0',
