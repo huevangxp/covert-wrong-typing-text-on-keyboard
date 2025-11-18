f5942b119cde30220ada02bacfe8c68d4411a11e
huevangxp <huevang933@gmail.com>
2025-11-18 17:58:42 +0700

    commit Tue 11/18/2025 17:58:42.21

--- app/pages/loading.vue
+++ app/pages/loading.vue
@@ -9,3 +9,24 @@
          </div>
     </div>
 </template>
+
+<style scoped>
+
+.loading{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100px;
+    height: 100px;
+    gap:6px;
+}
+
+.loading span{
+    width: 12px;
+    height: 12px;
+    background-color: #008080;
+    border-radius: 50%;
+    animation: loading 1s ease-in-out infinite;
+}
+
+</style>
