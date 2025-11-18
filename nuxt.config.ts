79f2ce6748625cb2a9fd5230fe893d279dcb8a3e
huevangxp <huevang933@gmail.com>
2025-11-18 15:44:08 +0700

    commit Tue 11/18/2025 15:44:08.17

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   css: ['vuetify/styles'],
+  plugins: ['@/plugins/vuetify.ts'],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   build: {
