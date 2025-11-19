67a09d94642234899dfb9e27680a467892772710
huevangxp <huevang933@gmail.com>
2025-11-19 10:14:30 +0700

    commit Wed 11/19/2025 10:14:30.18

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,7 +43,19 @@ export default defineNuxtConfig({
       },
     },
   },
-  
+  toast: {
+    position: 'top-center',
+    register: [ // Register custom toasts
+      {
+        name: 'my-error',
+        message: 'Oops...Something went wrong',
+        options: {
+          type: 'error'
+        }
+      }
+    ]
+},
+
   devServer: {
     port: 4040,
     host: '0.0.0.0',
