6a3f8cd786ee9b8f6ee546c320458d532ae9e184
huevangxp <huevang933@gmail.com>
2025-11-19 10:13:36 +0700

    commit Wed 11/19/2025 10:13:36.20

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,7 @@ export default defineNuxtConfig({
   },
   
   modules: [
+    '@nuxtjs/toast',
     (_options, nuxt) => {
       nuxt.hooks.hook('vite:extendConfig', (config) => {
         // @ts-expect-error
