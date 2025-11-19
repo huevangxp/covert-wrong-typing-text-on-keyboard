09b413c9a48eaccc3087cb3b7cdc253694e0c2d1
huevangxp <huevang933@gmail.com>
2025-11-19 09:45:11 +0700

    commit Wed 11/19/2025  9:45:11.21

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -12,8 +12,8 @@
             </v-col>
         </v-row>
         <div class="text-end">
-        <v-btn @click="convert" class="font-weight-bold " size="large" variant="elevated">ປ່ຽນພາສາອັງກິດ ຫາ ພາສາລາວ</v-btn>
-        <v-btn @click="convert" class="font-weight-bold " size="large" variant="elevated">ປ່ຽນພາສາລາວ ຫາ ພາສາອັງກິດ</v-btn>
+        <v-btn @click="convert"  size="large" variant="elevated">ປ່ຽນພາສາອັງກິດ ຫາ ພາສາລາວ</v-btn>
+        <v-btn @click="convert"  size="large" variant="elevated">ປ່ຽນພາສາລາວ ຫາ ພາສາອັງກິດ</v-btn>
 
         </div>
     </v-container>
