2df44c82ad7f121526b6bbe635232592a10ea87d
huevangxp <huevang933@gmail.com>
2026-01-20 15:38:19 +0700

    commit Tue 01/20/2026 15:38:19.14

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,43 +1,45 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 export default defineNuxtConfig({
   app: {
     head: {
-      titleTemplate: 'ແປປະໂຫຍກ ຫຼື ຂໍ້ຄວາມທີ່ພິມຜິດ',
-      title: 'ແປປະໂຫຍກ ຫຼື ຂໍ້ຄວາມທີ່ພິມຜິດ',
+      titleTemplate: "ແປປະໂຫຍກ ຫຼື ຂໍ້ຄວາມທີ່ພິມຜິດ",
+      title: "ແປປະໂຫຍກ ຫຼື ຂໍ້ຄວາມທີ່ພິມຜິດ",
       link: [
         {
-          rel: 'icon',
-          type: 'image/png',
-          href: '/logo.png',
+          rel: "icon",
+          type: "image/png",
+          href: "/logo.png",
         },
         {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+Lao:wght@100..900&display=swap',
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Noto+Sans+Lao:wght@100..900&display=swap",
         },
         {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap',
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap",
         },
       ],
     },
   },
-  compatibilityDate: '2025-11-19',
+  compatibilityDate: "2025-11-19",
   devtools: { enabled: true },
-  
+
   build: {
-    transpile: ['vuetify'],
+    transpile: ["vuetify"],
   },
-  
+
   modules: [
     (_options, nuxt) => {
-    nuxt.hooks.hook('vite:extendConfig', (config) => {
-      // @ts-expect-error
-      config.plugins.push(vuetify({ autoImport: true }))
-    })
-  }, 'nuxt-toast'],
-  
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+    "nuxt-toast",
+  ],
+
   vite: {
     vue: {
       template: {
@@ -45,10 +47,9 @@ export default defineNuxtConfig({
       },
     },
   },
- 
 
   devServer: {
     port: 4040,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
   },
-})
+});
