c6daf1f6162509ef653efe09156272e93231e7ac
huevangxp <huevang933@gmail.com>
2026-01-20 15:42:17 +0700

    commit Tue 01/20/2026 15:42:17.14

--- server/api/history.get.ts
+++ server/api/history.get.ts
@@ -1,4 +1,19 @@
 import { serverSupabaseClient } from "#supabase/server";
+
+export default defineEventHandler(async (event) => {
+  const client = await serverSupabaseClient(event);
+
+  // Get IP address
+  const xForwardedFor = getRequestHeader(event, "x-forwarded-for");
+  const ip = xForwardedFor
+    ? xForwardedFor.split(",")[0]
+    : event.node.req.socket.remoteAddress || "unknown";
+
+  const { data, error } = await client
+    .from("translation_history")
+    .select("*")
+    .eq("ip_address", ip)
+    .order("created_at", { ascending: false })
     .limit(10);
 
   if (error) {
