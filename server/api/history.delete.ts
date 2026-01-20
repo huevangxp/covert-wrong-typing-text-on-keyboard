55681ad1843cf4561b113851b3474cf6091c9aaf
huevangxp <huevang933@gmail.com>
2026-01-20 15:41:51 +0700

    commit Tue 01/20/2026 15:41:51.17

--- server/api/history.delete.ts
+++ server/api/history.delete.ts
@@ -1,4 +1,19 @@
 import { serverSupabaseClient } from "#supabase/server";
+
+export default defineEventHandler(async (event) => {
+  const client = await serverSupabaseClient(event);
+
+  // Get IP address
+  const xForwardedFor = getRequestHeader(event, "x-forwarded-for");
+  const ip = xForwardedFor
+    ? xForwardedFor.split(",")[0]
+    : event.node.req.socket.remoteAddress || "unknown";
+
+  const { error } = await client
+    .from("translation_history")
+    .delete()
+    .eq("ip_address", ip);
+
   if (error) {
     throw createError({
       statusCode: 500,
